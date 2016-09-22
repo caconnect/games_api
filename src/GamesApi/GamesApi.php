387f2cf2fe40b07857057b11cde91354b8d8f242
Alberto <alberto.burlacu93@gmail.com>
2016-09-22 13:22:09 +0300

    initial commit

---
+++ src/GamesApi/GamesApi.php
@@ -0,0 +1,41 @@
+<?php
+namespace Caconnect;
+use GamesApiExceptions;
+use Curl;
+
+class GamesApi {
+
+	private $apiUrl = 'http://api.licente-jocuri.ro/';
+	private $jwtAuth = null;
+	private $testMode = false;
+	private $loginCredentials = array();
+
+	private $_curlWrap;
+	private $_jwtToken;
+
+	public function __construct($username, $password)
+	{
+		if(!isset($username)) throw new InvalidUsername("You need to specify username to perform login action.");
+		$this->loginCredentials['username'] = filter_var($username, FILTER_SANITIZE_EMAIL);
+
+		if(!isset($password)) throw new InvalidPassword("You need to specify password to perform login action.");
+		
+		$this->_curlWrap = new Curl();
+
+
+		if(!isset($_SESSION['gamesApi_jwt'])) {
+			$this->_jwtToken = $this->obtainJwt(true);
+		} else {
+			$this->_jwtToken = $_SESSION['gamesApi_jwt'];
+		}
+
+		$this->_curlWrap->setHeader('Authorization', 'Bearer '.$_SESSION['gamesApi_jwt']);
+	}
+
+
+	private function obtainJwt($clean = false)
+	{
+		$this->_curlWrap->get($apiUrl.'/jwt');
+	}
+
+}
---
+++ src/GamesApi/GamesApiExceptions.php
@@ -0,0 +1,10 @@
+<?php
+namespace Caconnect;
+
+class InvalidUsername extends Exception { }
+
+class InvalidPassword extends Exception { }
+
+class InvalidCredentials extends Exception { }
+
+class ConnectionTimeOut extends Exception { }
